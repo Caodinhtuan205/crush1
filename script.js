e010d52217acc5b8edfb58ef061a92ffa6346310
Caodinhtuan205 <nguoilaoi20100111@gmail.com>
2024-10-08 13:26:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Anh yêu em, Anh hứa hongg làm em buồn nữa đouuu :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
+    "Anh biết lỗi của anh rùi ạa",
+    "Mong em iu tha lỗi choo anhh :((",
     "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "Em iuu đừng giận anhh nữa nhoo",
+    "Anh iu em nhắm lunnn đóoooo",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
